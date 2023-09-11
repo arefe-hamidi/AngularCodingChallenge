410a55cf2321c0d6eb84275fdb48755b235e62c4
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-09-11 13:29:00 +0330

    read json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularCodingChallenge';
+  numbersData: any;
+  url: string = '/assets/json/numbers.json';
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http.get(this.url).subscribe((res) => {
+      this.numbersData = res;
+      console.log(res);
+    });
+  }
 }
