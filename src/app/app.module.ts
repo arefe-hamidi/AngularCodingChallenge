03f348cdc77cbe4ec88fe14785494c5e391b3057
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-09-13 00:11:47 +0330

    add mat css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 import { OperationsComponent } from './operations/operations.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [AppComponent, HeaderComponent, OperationsComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
