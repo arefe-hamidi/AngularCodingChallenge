89979534ebcd538eebaf6c7297a5dd3802410f56
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-09-12 23:09:08 +0330

    read file

--- src/app/operations/operations.component.ts
+++ src/app/operations/operations.component.ts
@@ -7,12 +7,56 @@ import { HttpClient } from '@angular/common/http';
 })
 export class OperationsComponent implements OnInit {
   numbersData: any;
-  url: string = '/assets/json/numbers.json';
+  numberUrl: string = '/assets/json/numbers.json';
+  addData: any;
+  addUrl: string = '/assets/json/add.json';
+  multiplyData: any;
+  multiplyUrl: string = '/assets/json/multiply.json';
+  serverErrorStatus: boolean = false;
+  missingDataErrorStatus: boolean = false;
   constructor(private http: HttpClient) {}
   ngOnInit() {
-    this.http.get(this.url).subscribe((res) => {
-      this.numbersData = res;
-      console.log(res);
-    });
+    this.readNumbersData();
+    this.readAddData();
+    this.readMultiplyData();
+  }
+  readNumbersData() {
+    this.http.get(this.numberUrl).subscribe(
+      (res) => {
+        this.numbersData = res;
+      },
+      (error) => {
+        this.serverErrorStatus = true;
+        console.log('Server Error');
+      }
+    );
+  }
+  readAddData() {
+    this.http.get(this.addUrl).subscribe(
+      (res) => {
+        this.addData = res;
+        if (!this.addData.value) {
+          this.missingDataErrorStatus = true;
+        }
+      },
+      (error) => {
+        this.missingDataErrorStatus = true;
+        console.log('MISSING DATA');
+      }
+    );
+  }
+  readMultiplyData() {
+    this.http.get(this.multiplyUrl).subscribe(
+      (res) => {
+        this.multiplyData = res;
+        if (!this.multiplyData.value) {
+          this.missingDataErrorStatus = true;
+        }
+      },
+      (error) => {
+        this.missingDataErrorStatus = true;
+        console.log('MISSING DATA');
+      }
+    );
   }
 }
